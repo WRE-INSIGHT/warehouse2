4ea3bfb301005bd9b1f965e0be101b3b9413aad5
Ej1224 <39943828+Ej1224@users.noreply.github.com>
2019-11-13 13:41:13 +0800

    Change Credentials

--- warehouse2/warehouse2/Main/Forms/ChangeCredentialsFrm.vb
+++ warehouse2/warehouse2/Main/Forms/ChangeCredentialsFrm.vb
@@ -9,22 +9,77 @@
     End Sub
 
     Private Sub Save_btn_Click(sender As Object, e As EventArgs) Handles Save_btn.Click
-        If OldPassword_Tbox.Text = Nothing Then
-            KMDIPrompts(Me, "UserWarning",,,, True, True, "Old password required", False)
-            OldPassword_Tbox.Focus()
+        Try
+            If OldPassword_Tbox.Text = Nothing Then
+                KMDIPrompts(Me, "UserWarning",,,, True, True, "Old password required", False)
+                OldPassword_Tbox.Focus()
+                Exit Sub
 
-        ElseIf OldPassword_Tbox.Text <> pass Then
-            KMDIPrompts(Me, "UserWarning",,,, True, True, "Old password does not match", False)
-            OldPassword_Tbox.Focus()
+            ElseIf Password_Tbox.Text <> Retype_Tbox.Text Then
+                KMDIPrompts(Me, "UserWarning",,,, True, True, "Passwords does not match", False)
+                Password_Tbox.Focus()
+                Exit Sub
 
-        ElseIf Password_Tbox.Text <> Retype_Tbox.Text Then
-            KMDIPrompts(Me, "UserWarning",,,, True, True, "Passwords does not match", False)
-            Password_Tbox.Focus()
+            ElseIf OldPassword_Tbox.Text <> pass Then
+                KMDIPrompts(Me, "UserWarning",,,, True, True, "Old password does not match", False)
+                OldPassword_Tbox.Focus()
+                Exit Sub
 
-        ElseIf Password_Tbox.Text = Retype_Tbox.Text And OldPassword_Tbox.Text = pass Then
-            'KMDIPrompts(Me, "UserWarning",,,, True, True, "Passwords does not match", False)
-            MsgBox("success")
-        End If
+            ElseIf Password_Tbox.Text = Retype_Tbox.Text And OldPassword_Tbox.Text = pass Then
+                Dim fullname, nickname, username, password As String
+
+                If Fullname_Tbox.Enabled = True Then
+                    fullname = Trim(Fullname_Tbox.Text)
+                Else
+                    fullname = user_fullname
+                End If
+
+                If Nickname_Tbox.Enabled = True Then
+                    nickname = Trim(Nickname_Tbox.Text)
+                Else
+                    nickname = user_nick
+                End If
+
+                If Username_Tbox.Enabled = True Then
+                    username = Trim(Username_Tbox.Text)
+                    WarehouseMain_STP("warehouse_main_stp", "getuser",,, username,,)
+                    If sqlDataSet.Tables("getuser").Rows(0).Item(0) <> 0 Then
+                        KMDIPrompts(Me, "UserWarning",,,, True, True, "Exisiting username", True)
+                        Exit Sub
+                    End If
+                Else
+                    username = user
+                End If
+
+                If Password_Tbox.Enabled = True Then
+
+                    If Trim(Password_Tbox.Text) = Nothing Or Trim(Retype_Tbox.Text) = Nothing Then
+                        KMDIPrompts(Me, "UserWarning",,,, True, True, "Invalid password.", False)
+                        Password_Tbox.Focus()
+                        Exit Sub
+                    Else
+                        password = Trim(Password_Tbox.Text)
+
+                    End If
+                Else
+                    password = pass
+                End If
+
+                WarehouseMain_STP("warehouse_main_stp", "transchangecred", fullname, nickname, username, password, user_id)
+
+                If sql_Transaction_result = "Committed" Then
+                    KMDIPrompts(Me, "Success", Nothing, Nothing, Nothing, True,,, False)
+                    MessageBox.Show(Me, "Relogin to apply changes. ", "", MessageBoxButtons.OK, MessageBoxIcon.Exclamation)
+                    LoginFrm.Close()
+                Else
+                    KMDIPrompts(Me, "DotNetError", "", "", Nothing, True, True, "Failed", False)
+                End If
+
+
+            End If
+        Catch ex As Exception
+            KMDIPrompts(Me, "DotNetError", ex.Message, ex.StackTrace, Nothing, True)
+        End Try
     End Sub
 
     Private Sub Chk_CheckedChanged(sender As Object, e As EventArgs) Handles Fullname_Chk.CheckedChanged,
---
+++ warehouse2/warehouse2/Main/Modules/MainModule.vb
@@ -0,0 +1,45 @@
+﻿Imports System.Data.SqlClient
+Module MainModule
+
+    Public Sub WarehouseMain_STP(ByVal StoredProcedureName As String,
+                                 ByVal dsTbl_Command As String,
+                                 Optional fullname As String = "",
+                                 Optional nickname As String = "",
+                                 Optional username As String = "",
+                                 Optional password As String = "",
+                                 Optional id As Integer = 0)
+        sqlDataAdapter = New SqlDataAdapter
+        sqlBindingSource = New BindingSource
+        sqlDataSet = New DataSet
+        If sqlDataSet.Tables.Contains(dsTbl_Command) Then
+            sqlDataSet.Tables(dsTbl_Command).Clear()
+        End If
+        Using sqlcon As New SqlConnection(sqlconnString)
+            sqlcon.Open()
+            Using sqlCommand As SqlCommand = sqlcon.CreateCommand()
+                transaction = sqlcon.BeginTransaction(IsolationLevel.RepeatableRead, StoredProcedureName)
+                sqlCommand.Connection = sqlcon
+                sqlCommand.Transaction = transaction
+                sqlCommand.CommandText = StoredProcedureName
+                sqlCommand.CommandType = CommandType.StoredProcedure
+                sqlCommand.Parameters.Add("@todo", SqlDbType.VarChar).Value = dsTbl_Command
+                sqlCommand.Parameters.Add("@fullname", SqlDbType.VarChar).Value = fullname
+                sqlCommand.Parameters.Add("@nickname", SqlDbType.VarChar).Value = nickname
+                sqlCommand.Parameters.Add("@username", SqlDbType.VarChar).Value = username
+                sqlCommand.Parameters.Add("@password", SqlDbType.VarChar).Value = password
+                sqlCommand.Parameters.Add("@id", SqlDbType.Int).Value = id
+
+                sqlCommand.ExecuteNonQuery()
+                transaction.Commit()
+
+                sql_Transaction_result = "Committed"
+                If dsTbl_Command.Contains("trans") = False Then
+                    sqlDataAdapter.SelectCommand = sqlCommand
+                    sqlDataAdapter.Fill(sqlDataSet, dsTbl_Command)
+                    sqlBindingSource.DataSource = sqlDataSet
+                    sqlBindingSource.DataMember = dsTbl_Command
+                End If
+            End Using
+        End Using
+    End Sub
+End Module
