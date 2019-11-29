20ccf39dc0391e8707796c8fe8f024a1d7ccd7b3
Ej1224 <39943828+Ej1224@users.noreply.github.com>
2019-11-29 16:14:32 +0800

    5PM

--- warehouse2/warehouse2/Accountability/Forms/AccountabilityFrm.vb
+++ warehouse2/warehouse2/Accountability/Forms/AccountabilityFrm.vb
@@ -56,6 +56,7 @@ Public Class AccountabilityFrm
     End Sub
     Public Sub Start_BGW()
         Try
+
             If BGW.IsBusy <> True Then
                 DGV_Accountability.Enabled = False
                 DGV_Search_Inventory.Enabled = False
